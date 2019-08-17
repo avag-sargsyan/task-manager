1479f19ce0db9bf7e9cb9323ba0d1717a6d4e3c0
avag-sargsyan <avagsargsyan88@gmail.com>
2019-08-17 19:35:08 +0400

    Create basic components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import AppNavbar from './components/AppNavbar';
+import LoginModal from './components/LoginModal';
+import Users from './components/Users';
+import Reports from './components/Reports';
+import Notifications from './components/Notifications';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Task Manager</h1>
+      <AppNavbar />
+      <LoginModal />
+      <Users />
+      <Reports />
+      <Notifications />
     </div>
   );
 }
